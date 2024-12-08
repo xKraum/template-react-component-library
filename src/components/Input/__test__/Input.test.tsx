b15cd0bcd84cf7971649bc2d22d06218f71d082b
Jose Lacueva <xkraum@gmail.com>
2024-12-08 11:28:21 +0100

    - Add user event actions to tests. - Create Input component as library template placeholder. - Update README.

---
+++ src/components/Input/__test__/Input.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import { describe, expect, it, vitest } from 'vitest';
+
+import Input from '../Input';
+
+const renderInput = (value = '', onChange = vitest.fn()) => {
+  render(<Input value={value} onChange={onChange} />);
+  return screen.getByRole('textbox') as HTMLInputElement;
+};
+
+describe('Input component', () => {
+  it('Input should render correctly', () => {
+    const input = renderInput();
+    expect(input).toBeInTheDocument();
+  });
+
+  it('should call onChange for each character typed', async () => {
+    const user = userEvent.setup();
+    const handleChange = vitest.fn();
+    const textToInput = 'test';
+
+    const input = renderInput('', handleChange);
+    user.type(input, textToInput);
+
+    await waitFor(() =>
+      expect(handleChange).toHaveBeenCalledTimes(textToInput.length),
+    );
+  });
+});
