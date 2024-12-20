2023035f5d01a8a6ffa5afb4f742b6a5c002d32b
Jose Lacueva <xkraum@gmail.com>
2024-12-20 16:28:58 +0100

    fix: Correct component import.

--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import Button from '../Button';
+import { Button } from '../Button';
 
 describe('Button component', () => {
   it('Button should render correctly', () => {
