0ad34701aa187683fd58ec7e42557d58d5aeedd7
Jose L <58451072+jolacdev@users.noreply.github.com>
2024-12-26 14:05:18 +0100

    feat: Add 'disabled' prop and CSS interaction adjustments.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,15 +2,17 @@ import { MouseEventHandler } from 'react';
 import './Button.css';
 
 export type ButtonProps = {
+  disabled?: boolean;
   label?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
 export const Button = ({
+  disabled = false,
   label = 'Default',
   onClick = undefined,
 }: ButtonProps) => (
-  <button className="trcl" onClick={onClick}>
+  <button className="trcl" disabled={disabled} onClick={onClick}>
     {label}
   </button>
 );
