3c683a2d1f483d7c3242f791ebd75251fe820bd9
Jose L <58451072+jolacdev@users.noreply.github.com>
2024-12-26 17:42:24 +0100

    docs: Add Input examples and docs to the Storybook.

---
+++ src/components/Input/__docs__/Input.stories.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import type { Meta, StoryFn } from '@storybook/react';
+
+import { Input, InputProps } from './../Input';
+
+export default {
+  component: Input,
+  title: 'Input',
+} as Meta<typeof Input>;
+
+const Template: StoryFn<InputProps> = (props: InputProps) => {
+  const [value, setValue] = useState(props.value || '');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return <Input {...props} value={value} onChange={handleChange} />;
+};
+
+export const InputDefault = Template.bind({});
+InputDefault.args = {
+  placeholder: '',
+  value: '',
+};
+
+export const InputWithPlaceholder = Template.bind({});
+InputWithPlaceholder.args = {
+  placeholder: 'Search...',
+  value: '',
+};
+
+export const InputWithText = Template.bind({});
+InputWithText.args = {
+  placeholder: 'Search...',
+  value: 'Some text',
+};
