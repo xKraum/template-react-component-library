cd2bcc75e3c801d991567d1bdfdc026edda8a39e
Jose Lacueva <xkraum@gmail.com>
2024-12-29 12:20:57 +0100

    test

--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -4,7 +4,9 @@ import { describe, expect, it } from 'vitest';
 import { Button } from '../Button';
 
 describe('Button component', () => {
+  const testFormat1 = 0;
   it('Button should render correctly', () => {
+    const isTestLintError = true; // NOTE: Error?
     render(<Button />);
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
