04875725aede8773624d8be824d705fd880ab8ef
Jose Lacueva <xkraum@gmail.com>
2025-01-24 20:35:36 +0100

    Add ESLint parser and comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,58 +1,37 @@
+import perfectionist from 'eslint-plugin-perfectionist';
+import tsParser from '@typescript-eslint/parser';
 // eslint.config.js
 // https://medium.com/@1608naman/a-flat-attempt-at-the-eslint-flat-config-393005212d67
 // https://perfectionist.dev/guide/getting-started
-import { fileURLToPath } from 'url';
-import perfectionist from 'eslint-plugin-perfectionist';
-import { FlatCompat } from '@eslint/eslintrc';
-import path from 'path';
-import tsParser from '@typescript-eslint/parser';
-
-// Resolve __dirname in ESM
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname, // Optional, default to process.cwd()
-// });
-
-// Resolve __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname, // Optional, default to process.cwd()
-});
 
 export default [
-  // // Migrate env using FlatCompat
-  // ...compat.env({
-  //   browser: true,
-  //   node: true,
-  // }),
-
   {
-    files: ['src/**/*.{js,mjs,cjs,jsx,ts,tsx}', 'eslint.config.js'],
+    // files: ['src/**/*.{js,mjs,cjs,jsx,ts,tsx}', 'eslint.config.js'],
 
+    // PARSER: TypeScript & JSX support using the nearest tsconfig.json.
+    // https://eslint.org/docs/latest/use/configure/parser
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        project: true, // Source file being linted should use type information based on the nearest tsconfig.json.
         ecmaFeatures: {
           jsx: true,
         },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
-      // globals: {
-      //   ...globals.browser,
-      //   ...globals.node,
-      //   ...globals.serviceworker,
-      // },
     },
 
+    // PLUGINS: Set of rules that can be individually applied in the `rules` object.
+    // https://eslint.org/docs/latest/use/configure/plugins
     plugins: {
-      // '@typescript-eslint': typescript,
       perfectionist,
     },
+
+    // RULES: Defines the specific linting rules, can be included rules added in the `plugins` object.
+    // https://eslint.org/docs/latest/use/core-concepts/#rules
     rules: {
+      // NOTE: 0 = Off | 1 = Warn | 2 = Error
       'perfectionist/sort-imports': 2,
     },
   },
