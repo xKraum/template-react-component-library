5e88ab410f572845b97054d1b3af0d66d41a81b6
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-02-05 19:21:26 +0100

    fix: 🛠️ Exclude Storybook build directory from linting

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default [
 
   // GLOBAL IGNORES: Added in a separate object to apply globally and be able to match directories. Only global `ignores` can match directories.
   {
-    ignores: ['dist', 'build', '!.storybook'], // Ignore `dist` and `build` directories but prevent ignoring `.storybook`.
+    ignores: ['dist', 'build', 'storybook-static', '!.storybook'], // Ignore some directories but prevent ignoring `.storybook`.
   },
 
   // SETTINGS: Detect React version automatically for `eslint-plugin-react` rules.
