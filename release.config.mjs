49f5effd606c001a01e30e3197b5dbb622aaff37
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-02-19 17:56:24 +0100

    ci: 👷 Create CI and CD workflows
    * ci: :construction_worker: Add CI workflow for build, lint, and running tests
    * ci: :heavy_plus_sign: add semantic-release dependencies
    * ci: :construction_worker: Update CI to run on PR to specific branches
    * ci: :construction_worker: Add CD workflow with automatic semantic release
    * chore: :pushpin: Remove version carets

---
+++ release.config.mjs
@@ -0,0 +1,22 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['github-actions'],
+  // NOTE: commit-analyzer, release-notes-generator, npm and github are already part of @semantic-release.
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'README.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
